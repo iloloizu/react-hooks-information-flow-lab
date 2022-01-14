743f5ed966c3406c20e9465e2261d3bb97d3ac63
Ilolo Izu <iloloizu97@gmail.com>
2022-01-13 22:10:57 -0600

    complete assignments

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header({ isDarkMode, onDarkModeClick }) {
+    // function onDarkModeClick() {
+
+    // }
+    return (
+        <div>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+                {isDarkMode ? "Dark" : "Light"} Mode
+            </button>
+        </div>
+    )
+}
+
+export default Header;
